79a552d3df93a4669e4944fc8c0934b8865a74ba
Seongyun <okks3579@gmail.com>
2024-10-03 01:23:05 +0900

    refactor: Ball부문 줄이기

--- src/baseBall.java
+++ src/baseBall.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -18,57 +20,18 @@ public class baseBall {
                 int[] userArr = new int[3];
                 userNumberList(userNumber,userArr);
 
-                int strike = 0;
-                int ball = 0;
-                if (userArr[0] == randomArr[0]) {
-                    strike += 1;
-                    if (userArr[2] == randomArr[1]) {
-                        ball += 1;
-                    }
-                    if (userArr[1] == randomArr[2]) {
-                        ball += 1;
-                    }
-                }
-                if (userArr[1] == randomArr[1]) {
-                    strike +=1;
-                    if (userArr[0] == randomArr[2]) {
-                        ball += 1;
-                    }
-                    if (userArr[2] == randomArr[0]) {
-                        ball += 1;
-                    }
-                }
-                if (userArr[2] == randomArr[2]) {
-                    strike += 1;
-                    if (userArr[1] == randomArr[0]) {
-                        ball += 1;
-                    }
-                    if (userArr[0] == randomArr[1]) {
-                        ball += 1;
-                    }
-                }
-
+                List<Integer> strikeBall = strikeAndBall(userArr,randomArr);
+                int strike = strikeBall.get(0);
+                int ball = strikeBall.get(1);
 
                 if (strike==0) {
-                    if (userArr[0]==randomArr[1]) {
-                        ball+=1;
+                    for (int i=0;i<3;i++) {
+                        for (int j=0;j<3;j++) {
+                            if (i!=j && userArr[i]==randomArr[j]) {
+                                ball+=1;
+                            }
+                        }
                     }
-                    if (userArr[1]==randomArr[2]) {
-                        ball += 1;
-                    }
-                    if (userArr[0]==randomArr[2]) {
-                        ball+=1;
-                    }
-                    if (userArr[1]==randomArr[0]) {
-                        ball += 1;
-                    }
-                    if (userArr[2]==randomArr[0]) {
-                        ball += 1;
-                    }
-                    if (userArr[2]==randomArr[1]) {
-                        ball += 1;
-                    }
-
                 }
 
                 if (strike == 3) {
@@ -77,14 +40,14 @@ public class baseBall {
                 if (strike>0) {
                     if (ball > 0) {
                         System.out.println(strike + "스트라이크 " + ball + "볼");
+                    } else {
+                        System.out.println(strike + "스트라이크");
                     }
-                    System.out.println(strike + "스트라이크");
                 } else if (ball > 0) {
                     System.out.println(ball + "볼");
-
+                } else {
+                    System.out.println(strike + "스트라이크 " + ball + "볼");
                 }
-                System.out.print("strike " + strike+ " ");
-                System.out.println("ball " + ball);
             }
             System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임종료");
             System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
@@ -100,7 +63,7 @@ public class baseBall {
     private static int randomNumberPick() {
         Random random = new Random();
         random.setSeed(System.currentTimeMillis());
-        int randomNumber = random.nextInt(899) + 100;
+        int randomNumber = random.nextInt(899)+100;
         System.out.println(randomNumber);
         return randomNumber;
     }
@@ -128,5 +91,23 @@ public class baseBall {
             userNumber -= minus;
             userNumberset /= 10;
         }
+    }
+    private static List<Integer> strikeAndBall(int[] userArr, int[] randomArr) {
+        List<Integer> strikeBall = new ArrayList<>();
+        int strike = 0;
+        int ball = 0;
+        for (int i =0; i<3; i++) {
+            if (userArr[i] == randomArr[i]) {
+                strike += 1;
+                if (userArr[(i+2)%3]==randomArr[(i+1)%3] && userArr[(i+1)%3] == randomArr[(i+2)%3]) {
+                    ball += 2;
+                } else if (userArr[(i+2)%3]==randomArr[(i+1)%3] || userArr[(i+1)%3] == randomArr[(i+2)%3]) {
+                    ball += 1;
+                }
+            }
+        }
+        strikeBall.add(strike);
+        strikeBall.add(ball);
+        return strikeBall;
     }
 }
